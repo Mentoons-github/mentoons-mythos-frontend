b4e0217c8eee5b121ff50cc6a39021fe201160b4
Devanps <devanps212@gmail.com>
2025-07-15 11:28:47 +0530

    blog changes added

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,19 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice'
-import blogReducer from '../features/blog/blogSlice'
-import uploadReducer from '../features/upload/fileUploadSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import blogReducer from "../features/blog/blogSlice";
+import uploadReducer from "../features/upload/fileUploadSlice";
 import astroReducer from "../features/astrology/astroSlice";
+import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
-    reducer:{
-        auth:authReducer,
-        blog:blogReducer,
-        upload:uploadReducer,
-      astro: astroReducer,
-    }
-})
+  reducer: {
+    auth: authReducer,
+    blog: blogReducer,
+    upload: uploadReducer,
+    astro: astroReducer,
+    user: userReducer,
+  },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
