0201fb80dbd44361850c65aafff525c1f7285666
MohammedJasim001 <jasimat001@gmail.com>
2026-05-05 18:57:01 +0530

    resolve type errors

--- src/components/modal/BlogModal/Comment/CommetItem.tsx
+++ src/components/modal/BlogModal/Comment/CommetItem.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+// import { useState } from "react";
 import UserAvatar from "./UserAvatar";
-import CommentHeader from "./CommentHeader";
+// import CommentHeader from "./CommentHeader";
 import { Comments } from "../../../../types/redux/blogInterface";
-import CommentInput from "../CommentInput";
-import RepliesSection from "./ReplySection";
+// import CommentInput from "../CommentInput";
+// import RepliesSection from "./ReplySection";
 
 interface CommentItemProps {
   comment: Comments;
@@ -11,42 +11,42 @@ interface CommentItemProps {
 }
 
 const CommentItem: React.FC<CommentItemProps> = ({ comment, onReply }) => {
-  const [replyingTo, setReplyingTo] = useState<string | null>(null);
+  // const [replyingTo, setReplyingTo] = useState<string | null>(null);
+console.log(onReply)
+  // const handleReplySubmit = (text: string) => {
+  //   onReply(comment._id, text);
+  //   setReplyingTo(null);
+  // };
 
-  const handleReplySubmit = (text: string) => {
-    onReply(comment._id, text);
-    setReplyingTo(null);
-  };
-
-  const toggleReply = () => {
-    setReplyingTo(replyingTo === comment._id ? null : comment._id);
-  };
+  // const toggleReply = () => {
+  //   setReplyingTo(replyingTo === comment._id ? null : comment._id);
+  // };
 
   return (
     <li className="flex items-start gap-3 px-3 py- rounded-md">
-      <UserAvatar profile={comment.profile} username={comment.username} />
+      <UserAvatar profile={comment._id} username={comment.comment} />
 
       <div className="flex-1">
-        <CommentHeader
+        {/* <CommentHeader
           commentId={comment._id}
-          userId={comment.userId}
-          username={comment.username}
-          createdAt={comment.createdAt}
-          onReply={toggleReply}
-        />
+          // userId={comment.}
+          // username={comment.blogId}
+          // createdAt={comment.createdAt}
+          // onReply={toggleReply}
+        /> */}
 
         <p className="text-sm  mt-1">{comment.comment}</p>
 
-        {replyingTo === comment._id && (
+        {/* {replyingTo === comment._id && (
           <CommentInput
             onCancel={() => setReplyingTo(null)}
             isOpen={true}
             onSubmit={handleReplySubmit}
             placeholder={`Replying to ${comment.username}`}
           />
-        )}
+        )} */}
 
-        <RepliesSection replies={comment.reply} commentId={comment._id} />
+        {/* <RepliesSection replies={comment.reply} commentId={comment._id} /> */}
       </div>
     </li>
   );
