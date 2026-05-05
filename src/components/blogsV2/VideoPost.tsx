19eed1eaffb7a96245a501088b6a7a2adb06f31e
MohammedJasim001 <jasimat001@gmail.com>
2026-05-05 18:59:04 +0530

    resolve type errors

--- src/components/blogsV2/VideoPost.tsx
+++ src/components/blogsV2/VideoPost.tsx
@@ -1,23 +1,23 @@
-import { IBlogV2 } from "../../types/redux/blogInterface";
-import PostWrapper from "./PostWrapper";
+// import { IBlogV2 } from "../../types/redux/blogInterface";
+// import PostWrapper from "./PostWrapper";
 
-const VideoPost = ({ post }: { post: IBlogV2 }) => {
-  const media = post?.media?.[0];
+// const VideoPost = ({ post }: { post: IBlogV2 }) => {
+//   const media = post?.media?.[0];
 
-  if (!media?.url) return null;
+//   if (!media?.url) return null;
 
-  return (
-    <PostWrapper post={post}>
-      {media?.url && (
-        <video
-          src={media.url}
-          controls
-          className="w-full max-h-[400px] rounded-xl"
-        />
-      )}
-      {media?.caption && <p className="text-sm mt-2">{media.caption}</p>}
-    </PostWrapper>
-  );
-};
+//   return (
+//     <PostWrapper post={post}>
+//       {media?.url && (
+//         <video
+//           src={media.url}
+//           controls
+//           className="w-full max-h-[400px] rounded-xl"
+//         />
+//       )}
+//       {media?.caption && <p className="text-sm mt-2">{media.caption}</p>}
+//     </PostWrapper>
+//   );
+// };
 
-export default VideoPost;
+// export default VideoPost;
