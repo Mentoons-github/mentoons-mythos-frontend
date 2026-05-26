527c1d686baa1214bd2970a3a83954ec678fac0e
MohammedJasim001 <jasimat001@gmail.com>
2026-05-26 18:19:44 +0530

    fix react error

--- src/Admin/components/Workshop/AddWorkshopPlan.tsx
+++ src/Admin/components/Workshop/AddWorkshopPlan.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import CreateWorkshopPlanModal from "../modals/Workshop/CreateWorkshopPlanModal";
 
 const AddWorkshopPlan = () => {
